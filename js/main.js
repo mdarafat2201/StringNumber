bd6cadb3978bc74ff14facf9986be666a442a861
mdarafat2201 <maccenarafat@gmail.com>
2024-01-12 11:31:08 +0000

    string number

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/*const output = document.querySelector("#output");
+const Gbtn = document.querySelector(".Gbtn");
+
+function isString(value) {
+  return value.constructor === String;
+}
+
+Gbtn.addEventListener("click", function () {
+  if (isString(output.value)) {
+    console.log("this is string");
+  } else {
+    console.log("this is number");
+  }
+});*
+const output = document.querySelector("#output");
+const Gbtn = document.querySelector(".Gbtn");
+
+Gbtn.addEventListener("click", function () {
+  if (isNaN(output.value)) {
+    console.log("string");
+  } else {
+    console.log("number");
+  }
+});
+*/
